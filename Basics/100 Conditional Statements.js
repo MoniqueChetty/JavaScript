59f04da4046430d35be50ca42d775ae745d3be61
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2020-07-15 21:59:45 +0200

    Conditional Statement

--- Basics/100 Conditional Statements.js
+++ Basics/100 Conditional Statements.js
@@ -15,3 +15,31 @@ if (5 < 7) {
 
 console.clear();
 //switch
+
+var currentDay = 'Mon';
+var currentDay = 'Fri';
+
+switch (currentDay) {
+    case 'Mon':
+        console.log('Monday Timings: 10:00 - 06:00');
+        break;
+    case 'Tue':
+        console.log('Tuesday Timings: 09:00 - 05:00');
+        break;
+    case 'Wed':
+        console.log('Wednesday Timings: 09:00 - 05:00');
+        break;
+    case 'Thu':
+        console.log('Thursday Timings: 09:00 - 05:00');
+        break;
+    case 'Fri':
+        console.log('Friday Timings: 09:00 - 05:00');
+        break;
+    case 'Sat':
+        console.log('Saturday Timings: 10:00 - 06:00');
+        break;
+    case 'Sun':
+        console.log('Sunday Timings: 10:00 - 06:00');
+        break;
+
+}
