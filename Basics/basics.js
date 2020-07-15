701c49b97bde2c3d6feb71a15bd7a3f2e5a1ed26
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2020-07-15 20:49:16 +0200

    Basics

--- Basics/basics.js
+++ Basics/basics.js
@@ -45,9 +45,79 @@ console.log(strFloat.toFixed(14));
 
 console.clear();
 
-//  96. JS Strings/////////////////////////////////////////////////////////////
+// 96. JS Strings//////////////////////////////////////////////////////
 
-var my1stString = "I Love Javascript";
+var my1stString = "I Love \"Javascript\" ";
 console.log(typeof(myFirstString));
 
-var my2ndString = " We will learn JS together";
+var my2ndString = 'We will learn JS together';
+
+console.clear();
+//97. JS Building String Functions//////////////////
+var my1stString2 = "This is a string for JavaScript string functions, Javascript";
+
+console.log(my1stString2.length);
+
+//Find index of string inside another string
+console.log(my1stString2.indexOf('JavaScript'));
+console.log(my1stString2.indexOf('This'));
+
+//Find Last index of sting in anoth String
+console.log(my1stString2.lastIndexOf('Javascript'));
+console.log(my1stString2.lastIndexOf('This'));
+
+//part of a String slice(start, end)
+console.log(my1stString2.slice(0, 4));
+console.log(my1stString2.slice(21, 31));
+console.log(my1stString2.slice(-10));
+console.log(my1stString2.slice(5));
+
+// sub string :- substr(startPos, length)
+console.log(my1stString2.substr(0, 4));
+console.log(my1stString2.substr(21, 10));
+console.log(my1stString2.substr(21));
+
+console.clear();
+
+// 98. JS Sting Funstions/////////////////////////
+
+var exampleString = "This is Javascript Tutorial";
+
+console.log(exampleString.toUpperCase());
+console.log(exampleString.toLowerCase());
+
+//concat
+var firstName = "Javascript";
+var lastName = "Playground";
+console.log(exampleString.concat(firstName));
+console.log(exampleString.concat(' ', firstName, ' ', lastName));
+console.log(firstName + ' ' + lastName + ' ' + exampleString);
+
+
+//trim()
+var space = '     run run run      ';
+console.log(space.trim());
+
+//charAt()
+var charAtExample = 'This is my test string';
+console.log(charAtExample.charAt(5));
+console.log(charAtExample.charAt(11));
+
+//split()
+var sampleString = 'This is my sample string';
+console.log(sampleString.split(' '));
+
+var sampleString2 = 'This,is,my,sample,string';
+console.log(sampleString2.split(','));
+
+var sampleString3 = 'This is awesome';
+console.log(sampleString3.split()); // does nothing
+
+console.clear();
+//null and undefined
+
+var mVar //mvar holds no value
+console.log(mVar); //undefined
+
+mVar = null;
+console.log(mVar); //null
