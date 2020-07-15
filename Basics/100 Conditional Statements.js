89a7db8409ec13ae868e665a7dec16e6ff871857
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2020-07-15 20:49:39 +0200

    Conditional Statements

---
+++ Basics/100 Conditional Statements.js
@@ -0,0 +1,17 @@
+// if / else
+if (7 < 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if (5 < 7) {
+    console.log(5 < 7); // true
+} else if (7 > 8) {
+    console.log('7 > 8'); //'7>8' String
+} else {
+    console.log("Nothing");
+}
+
+console.clear();
+//switch
