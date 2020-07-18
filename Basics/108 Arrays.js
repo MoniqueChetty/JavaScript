616387d8495ffd20b8c1d3737dac65addb3be654
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2020-07-18 11:53:40 +0200

    Update 108 Arrays.js

--- Basics/108 Arrays.js
+++ Basics/108 Arrays.js
@@ -36,5 +36,38 @@ var mName = mFriends.pop();
 console.log(mName);
 console.log(mFriends);
 
+console.clear();
 
 //109 Arrays2
+
+var mFriends2 = ['Ann', 'Sam', 'John', 'Peter', 'Ivan', 'James', 'Tom'];
+mFriends2.splice(3, 0, "Andy", "Sandy");
+
+//1st  - start position
+//2nd - how many to delete
+//3rd, 4th..... - items to add
+console.log(mFriends2);
+
+//From 0 delete 1 add Bill and Maison
+mFriends2.splice(0, 1, "Bill", "Maison");
+console.log(mFriends2);
+
+//From 0 delete 1 
+mFriends2.splice(3, 2);
+console.log(mFriends2);
+
+console.clear();
+// Concatenation
+
+var officeFriends = ["Tracy", " Steven", "Kathy", "Jannine"]
+
+var allFriends = mFriends2.concat(officeFriends);
+
+console.log(mFriends2)
+console.log(officeFriends)
+console.log(allFriends);
+
+// Sort
+console.log(allFriends.sort());
+console.log(allFriends);
+console.log(allFriends.reverse());
