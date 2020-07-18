56f962bdc1b4667f84c7b78a60163247ddb2a46a
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2020-07-18 09:53:35 +0200

    Create 108 Arrays.js

---
+++ Basics/108 Arrays.js
@@ -0,0 +1,40 @@
+//108 Arrays
+
+var mFriends = ['Ann', 'Sam', 'John', 'Peter', 'Ivan', 'James', 'Tom'];
+console.log(mFriends);
+console.log(typeof(mFriends));
+
+console.log(mFriends[2]);
+console.log(mFriends[5]);
+console.log(mFriends[10]);
+
+// Update
+mFriends[0] = "Anny";
+console.log(mFriends);
+
+//Add
+mFriends[8] = "Jesse";
+console.log(mFriends);
+
+mFriends[mFriends.length] = "Keeno";
+console.log(mFriends);
+
+mFriends.push("Maison");
+console.log(mFriends);
+
+//Delete
+var mName = mFriends.pop();
+console.log(mName);
+console.log(mFriends);
+var mName = mFriends.pop();
+console.log(mName);
+console.log(mFriends);
+var mName = mFriends.pop();
+console.log(mName);
+console.log(mFriends);
+var mName = mFriends.pop();
+console.log(mName);
+console.log(mFriends);
+
+
+//109 Arrays2
