95df0d0bc688f846bc1d1be6c5e48bb2fc2642e7
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2020-07-25 12:48:54 +0200

    Create index.js

---
+++ Lesson 118 DOM/index.js
@@ -0,0 +1,12 @@
+console.log('JS Loaded');
+console.log(document.getElementById("topbar"));
+console.log(document.getElementById("tree-logo"));
+
+console.log(document.getElementsByClassName("menu"));
+console.log(document.getElementsByTagName("img"));
+console.log(document.getElementsByTagName("p"))
+
+console.clear();
+//120 Query selector
+//console.log(document.querySelectorAll('.menu'));
+console.log(document.querySelector('.menu'));
