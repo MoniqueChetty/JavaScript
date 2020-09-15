6d76234224ea4e4a3d64d224847285209233370d
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2020-09-15 20:23:38 +0200

    Create script.js

---
+++ Lesson 150 JSON/script.js
@@ -0,0 +1,38 @@
+var mObj = {
+    "name": 'John',
+    "age": 28,
+    "married": false,
+    score: [22, 25, 12, 22, 32],
+    score2: {
+        "math": 25,
+        "science": 24
+    },
+
+}
+console.log(mObj);
+console.log("Name " + mObj.name);
+
+var strJson = JSON.stringify(mObj)
+console.log(strJson);
+console.log(JSON.parse(strJson));
+
+///////////////////////
+
+var mArr = [{
+        "name": 'John',
+        "age": 28
+    },
+    {
+        "name": 'Sam',
+        "age": 22
+    },
+    {
+        "name": 'Ann',
+        "age": 32
+    }
+]
+console.log(mArr[2].name + " age " + mArr[2].age);
+
+var strJson = JSON.stringify(mObj)
+console.log(strJson);
+console.log(JSON.parse(strJson));
